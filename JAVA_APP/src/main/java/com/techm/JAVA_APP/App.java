177b8530f40fbd9afa273f0f351f8b43d9d34eb9
Administrator <Administrator@172.31.95.86>
2022-05-27 13:54:02 +0000

    Initial master

---
+++ JAVA_APP/src/main/java/com/techm/JAVA_APP/App.java
@@ -0,0 +1,13 @@
+package com.techm.JAVA_APP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
